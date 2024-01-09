ca5aa6c293d05abaf0398d39a367bf26a68e1647
Uremisan <manfrededun@gmail.com>
2024-01-09 22:32:25 +0000

    first initialization

---
+++ taskmate/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from todolist import views
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.index, name = 'index'),
+    # define todolist url
+    path("todolist/", include("todolist.urls")),
+    path("account/", include("users_app.urls")),
+    path("contact", views.contact, name = 'contact'),
+    path("about", views.about, name = 'about')
+]
